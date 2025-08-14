d4022c00b4a478c34783a1bd3a581135cfc2ad19
valery <12847668+smukm@users.noreply.github.com>
2025-08-14 08:11:03 +0300

    next stage

--- doc/curl.sh
+++ doc/curl.sh
@@ -3,7 +3,7 @@
 for i in {1..10}; do
   echo "Request $i:"
   time curl -X POST http://localhost:8033/api/screen \
-    -H "Authorization: Bearer 12345" \
+    -H "Authorization: Bearer secret" \
     -F "html='<h1>Test $i</h1>'" \
     -F "browser=firefox" \
     -w "\nTime: %{time_total}s\nStatus: %{http_code}\n"
